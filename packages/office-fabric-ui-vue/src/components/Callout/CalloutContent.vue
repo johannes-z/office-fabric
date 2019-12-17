a3761c1dda36b3157fa567ba74c16aa2b6cf2dab
johannes-z <johannes.zwirchmayr@gmail.com>
2019-12-17 15:51:46 +0100

    fix callout

--- packages/office-fabric-ui-vue/src/components/Callout/CalloutContent.vue
+++ packages/office-fabric-ui-vue/src/components/Callout/CalloutContent.vue
@@ -1,10 +1,11 @@
 <template>
-  <div ref="callout"
-       :class="classNames.container">
-    <div :class="classNames.root" :style="{ left: left, top: top }">
+  <div ref="hostElement" :class="classNames.container">
+    <div ref="calloutElement"
+         :class="classNames.root"
+         :style="positionCss ? positionCss.elementPosition : null">
       <div v-if="isBeakVisible"
            :class="classNames.beak"
-           :style="beakPosition" />
+           :style="positionCss && positionCss.beakPosition ? positionCss.beakPosition.elementPosition : null" />
       <div v-if="isBeakVisible" :class="classNames.beakCurtain" />
       <div :class="classNames.calloutMain">
         <slot />
@@ -16,10 +17,12 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import BaseComponent from '../BaseComponent'
-import { classNamesFunction } from '@fabric-vue/utilities'
+import { classNamesFunction, getWindow, getDocument, ICalloutPositionedInfo, assign, IRectangle, IPosition } from '@fabric-vue/utilities'
 import { getStyles } from './CalloutContent.styles'
 import { concatStyleSetsWithProps } from '@uifabric/merge-styles'
 import { clickedOutside } from '@/util'
+
+import { positionCallout } from '@/utilities/positioning/'
 
 const getClassNames = classNamesFunction()
 
@@ -28,75 +31,129 @@ const getClassNames = classNamesFunction()
 })
 export default class CalloutContent extends BaseComponent {
   $refs!: {
-    callout: HTMLDivElement
+    hostElement: HTMLDivElement
+    calloutElement: HTMLDivElement
   }
 
   @Prop({ type: HTMLElement, required: true }) target!: HTMLElement
   @Prop() calloutWidth!: number
   @Prop({ default: 16 }) beakWidth!: number
+  @Prop({ default: 8 }) minPagePadding!: number
   @Prop({ default: true }) isBeakVisible!: boolean
   @Prop({ default: false }) coverTarget!: boolean
   @Prop({ default: 0 }) gapSpace!: number
 
   private internalKey = new Date()
+  private positions: any = null
+
+  positionAttempts = 0;
 
   created () {
     window.addEventListener('click', this.onGlobalClick, true)
+    window.addEventListener('scroll', this.onGlobalScroll, true)
   }
 
   beforeDestroy () {
     window.removeEventListener('click', this.onGlobalClick, true)
+    window.removeEventListener('scroll', this.onGlobalScroll, true)
   }
 
   updated () {
     this.internalKey = new Date()
   }
 
+  async mounted () {
+    this.updatePosition()
+  }
+
   get classNames (): any {
-    const { theme, className, styles, calloutWidth, beakWidth } = this
+    const { theme, className, positions, styles, calloutWidth, beakWidth } = this
 
     return getClassNames(concatStyleSetsWithProps({
-      theme,
-      className,
-      calloutWidth,
-      beakWidth,
+      theme, className, overflowYHidden: false, calloutWidth, beakWidth, backgroundColor: '#fff', calloutMaxWidth: '100%',
     }, getStyles, styles))
-  }
-
-  get targetRect (): ClientRect {
-    return this.internalKey && this.target.getBoundingClientRect()
-  }
-
-  get top () {
-    const beakOffset = this.isBeakVisible ? (this.actualBeakWidth / 2) : 0
-    const targetOffset = !this.coverTarget ? this.targetRect.height : 0
-    return `${this.targetRect.top + this.gapSpace + targetOffset + beakOffset}px`
-  }
-
-  get left () {
-    return this.targetRect.left + 'px'
   }
 
   get actualBeakWidth () {
     return Math.sqrt(this.beakWidth * this.beakWidth * 2)
   }
 
-  private get beakPosition () {
-    return {
-      left: `${this.targetRect.width / 2 - this.beakWidth / 2}px`,
-      top: `${-this.beakWidth / 2}px`,
+  private get positionCss () {
+    if (!this.positions) return null
+    const positionCss = this.positions
+    for (const key in positionCss.elementPosition) {
+      positionCss.elementPosition[key] = `${positionCss.elementPosition[key]}px`
     }
+    for (const key in positionCss.beakPosition.elementPosition) {
+      positionCss.beakPosition.elementPosition[key] = `${positionCss.beakPosition.elementPosition[key]}px`
+    }
+    return positionCss
   }
 
-  private get positions () {
-    return {
+  private updatePosition () {
+    const positions = this.positions
 
+    let currentProps: any
+    currentProps = assign(currentProps, this.$props)
+    currentProps!.bounds = this.bounds
+    currentProps!.target = this.target!
+    const newPositions = positionCallout(currentProps, this.$refs.hostElement, this.$refs.calloutElement, this.positions)
+
+    if (
+      (!positions && newPositions) ||
+        (positions && newPositions && !this._arePositionsEqual(positions, newPositions) && this.positionAttempts < 5)
+    ) {
+      this.positionAttempts++
+      this.positions = newPositions
+    } else if (this.positionAttempts > 0) {
+      this.positionAttempts = 0
     }
   }
 
   private onGlobalClick (e: Event) {
-    const outside = clickedOutside(e, this.$refs.callout)
+    const outside = clickedOutside(e, this.$refs.calloutElement)
     if (outside) this.$emit('dismiss', true)
+  }
+
+  private onGlobalScroll (e: Event) {
+    this.$emit('dismiss', true)
+  }
+
+  private get bounds (): IRectangle {
+    return {
+      top: 0 + this.minPagePadding!,
+      left: 0 + this.minPagePadding!,
+      right: window.innerWidth - this.minPagePadding!,
+      bottom: window.innerHeight - this.minPagePadding!,
+      width: window.innerWidth - this.minPagePadding! * 2,
+      height: window.innerHeight - this.minPagePadding! * 2,
+    }
+  }
+
+  private _arePositionsEqual (positions: ICalloutPositionedInfo, newPosition: ICalloutPositionedInfo): boolean {
+    return (
+      this._comparePositions(positions.elementPosition, newPosition.elementPosition) &&
+      this._comparePositions(positions.beakPosition.elementPosition, newPosition.beakPosition.elementPosition)
+    )
+  }
+
+  private _comparePositions (oldPositions: IPosition, newPositions: IPosition): boolean {
+    for (const key in newPositions) {
+      // This needs to be checked here and below because there is a linting error if for in does not immediately have an if statement
+      if (newPositions.hasOwnProperty(key)) {
+        const oldPositionEdge = oldPositions[key]
+        const newPositionEdge = newPositions[key]
+
+        if (oldPositionEdge !== undefined && newPositionEdge !== undefined) {
+          if (oldPositionEdge.toFixed(2) !== newPositionEdge.toFixed(2)) {
+            return false
+          }
+        } else {
+          return false
+        }
+      }
+    }
+    return true
   }
 }
 </script>
--- packages/office-fabric-ui-vue/src/pages/Callout.vue
+++ packages/office-fabric-ui-vue/src/pages/Callout.vue
@@ -3,6 +3,11 @@
     <h1>Callout</h1>
     <div class="content--inner ms-depth-8">
       <h2>Overview</h2>
+      <o-text block variant="large">Callouts are a powerful way to simplify a user interface. They host tips and other information users need when they need it, with minimal effort on their part. Callouts can help you use screen space more effectively and reduce screen clutter. However, poorly designed Callouts can be annoying, distracting, unhelpful, overwhelming, or in the way.</o-text>
+      <br>
+      <o-text block variant="large">Use a Callout for displaying additional contextual information about an item on the screen. Callouts also have a tail that identifies their source. A common use for Callout is the introduction of a new feature or capability of an app or site. Alternate usages include pairing the Callout with a button or clickable element for on-demand presentation of additional or supporting content.</o-text>
+      <br>
+      <o-text block variant="large">By default, Callouts that do not contain focusable elements (links, buttons etc) cannot gain focus when opened. For proper screen reader support, follow the non-focusable callout example, which treats the callout content like a status message.</o-text>
     </div>
 
     <div class="content--inner ms-depth-8">
@@ -31,6 +36,13 @@
           <p :class="$style.subtext">
             Message body is optional. If help documentation is available, consider adding a link to learn more at the bottom.
           </p>
+          <div :class="$style.actions">
+            <o-link :class="$style.link"
+                    href="http://microsoft.com"
+                    target="_blank">
+              Go to microsoft
+            </o-link>
+          </div>
         </div>
       </o-callout>
     </div>
@@ -90,5 +102,13 @@ export default class CalloutPage extends Vue {
   margin-bottom: 0px;
   margin-left: 0px;
   color: rgb(50, 49, 48);
+}
+.actions {
+  position: relative;
+  margin-top: 20px;
+  width: 100%;
+  white-space: nowrap;
+}
+.link {
 }
 </style>
