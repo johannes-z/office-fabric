924f001742f1b1169454c93b6efff8aabc24751e
johannes-z <johannes.zwirchmayr@gmail.com>
2020-01-27 16:57:29 +0100

    fix builds fix exports

--- packages/office-ui-fabric-vue/vue.config.js
+++ packages/office-ui-fabric-vue/vue.config.js
@@ -1,13 +1,9 @@
 const path = require('path')
-const alias = require('@rollup/plugin-alias')
-
-const projectRoot = path.resolve(__dirname)
 
 module.exports = {
   configureWebpack: {
     output: {
       library: 'OfficeUIFabric',
-      libraryTarget: 'var',
     },
     resolve: {
       symlinks: false,
