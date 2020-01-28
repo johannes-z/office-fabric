6541b0658c74781a35ab9d01faaf0837edefaa62
johannes-z <johannes.zwirchmayr@gmail.com>
2020-01-28 12:35:49 +0100

    fix BaseButton

--- packages/office-ui-fabric-vue/src/components/Button/BaseButton.vue
+++ packages/office-ui-fabric-vue/src/components/Button/BaseButton.vue
@@ -25,9 +25,9 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import BaseComponent from '../BaseComponent'
-import { getBaseButtonClassNames } from './BaseButton.classNames'
-import { getStyles } from './BaseButton.styles'
+import { getBaseButtonClassNames, IButtonClassNames } from './BaseButton.classNames'
 import { Icon } from '../Icon'
+import { ITheme } from '@uifabric/styling'
 
 const TouchIdleDelay = 500 /* ms */
 
@@ -37,10 +37,30 @@ const TouchIdleDelay = 500 /* ms */
 export default class BaseButton extends BaseComponent<any, any> {
   @Prop({ type: String, default: null }) href!: string
   @Prop({ type: Boolean, default: false }) checked!: boolean
+  @Prop({ type: Boolean, default: false }) split!: boolean
   @Prop({ type: Boolean, default: false }) disabled!: boolean
+  @Prop({ type: Boolean, default: false }) primaryDisabled!: boolean
+  @Prop({ type: Boolean, default: false }) allowDisabledFocus!: boolean
   @Prop({ type: String, default: null }) variantClassName!: string
-  @Prop({ type: Object, default: null }) iconProps!: any
+  @Prop({ type: Object, default: () => {} }) iconProps!: any
+  @Prop({ type: Object, default: () => {} }) menuIconProps!: any
+  @Prop({ type: Object, default: null }) menuProps!: any
   @Prop({ type: String, default: null }) secondaryText!: string
+  @Prop({ type: Function, default: null }) getClassNames!: (
+    theme: ITheme,
+    className: string,
+    variantClassName: string,
+    iconClassName: string | undefined,
+    menuIconClassName: string | undefined,
+    disabled: boolean,
+    checked: boolean,
+    expanded: boolean,
+    hasMenu: boolean,
+    isSplit: boolean | undefined,
+    allowDisabledFocus: boolean
+  ) => IButtonClassNames;
+
+  menuHidden: boolean = true
 
   get component (): 'a' | 'button' {
     const { disabled, href } = this
@@ -49,20 +69,36 @@ export default class BaseButton extends BaseComponent<any, any> {
   }
 
   get classNames (): any {
-    const { theme, styles, className, iconProps, variantClassName, disabled, checked } = this
-    return getBaseButtonClassNames(
-      theme,
-      styles,
-      className!,
-      variantClassName,
-      iconProps && iconProps.className,
-      undefined,
-      disabled,
-      false,
-      checked,
-      false,
-      false
-    )
+    const { theme, styles, className, iconProps, menuIconProps, variantClassName, disabled, primaryDisabled, checked, getClassNames } = this
+    const isPrimaryButtonDisabled = disabled || primaryDisabled
+
+    return getClassNames
+      ? getClassNames(
+        theme!,
+        className!,
+        variantClassName!,
+        iconProps && iconProps.className,
+        menuIconProps && menuIconProps.className,
+        isPrimaryButtonDisabled!,
+        checked!,
+        !this.menuHidden,
+        !!this.menuProps,
+        this.split,
+        !!this.allowDisabledFocus
+      )
+      : getBaseButtonClassNames(
+        theme!,
+        styles!,
+        className!,
+        variantClassName!,
+        iconProps && iconProps.className,
+        menuIconProps && menuIconProps.className,
+        isPrimaryButtonDisabled!,
+        !!this.menuProps,
+        checked!,
+        !this.menuHidden,
+        this.split
+      )
   }
 }
 </script>
