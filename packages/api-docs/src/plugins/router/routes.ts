e225fe12c3214bcefece35c4927d50d1ecda2044
johannes-z <johannes.zwirchmayr@gmail.com>
2020-03-02 11:08:33 +0100

    update public path

--- packages/api-docs/src/plugins/router/routes.ts
+++ packages/api-docs/src/plugins/router/routes.ts
@@ -1,17 +1,21 @@
 import { FabricComponents } from '@uifabric-vue/office-ui-fabric-vue'
 
+const publicPath = process.env.NODE_ENV === 'production'
+  ? '/office-fabric/'
+  : '/'
+
 const routes: any[] = []
 for (const name in FabricComponents) {
   routes.push({
     name,
-    path: `/${name}`,
+    path: `${publicPath}/${name}`,
     component: () => import(`@/pages/${name}.vue`),
     props: {},
   })
 }
 routes.push({
   name: 'Button',
-  path: `/Button`,
+  path: `${publicPath}/Button`,
   component: () => import(`@/pages/Button.vue`),
   props: {},
 })
--- packages/api-docs/vue.config.js
+++ packages/api-docs/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/office-fabric/'
+    : '/',
   configureWebpack: {
     resolve: {
       symlinks: false,
