7b5d22421a094673e9f282127ad4d5ef28bb00c5
johannes-z <johannes.zwirchmayr@gmail.com>
2020-03-18 13:57:08 +0100

    fix activity item

--- packages/office-ui-fabric-vue/src/components/ActivityItem/ActivityItem.vue
+++ packages/office-ui-fabric-vue/src/components/ActivityItem/ActivityItem.vue
@@ -1,16 +1,27 @@
 <template>
   <div :class="classNames.root">
-    <div :class="classNames.activityTypeIcon">
-      <slot name="icon" />
+    <div v-if="activityPersonas || $slots.icon" :class="classNames.activityTypeIcon">
+      <div v-if="animateBeaconSignal && isCompact" :class="classNames.pulsingBeacon" />
+
+      <div v-if="activityPersonas.length > 0" :class="classNames.personaContainer">
+        <PersonaCoin v-for="(person, index) in personasToRender"
+                     :key="person.key || index"
+                     v-bind="person"
+                     :class="classNames.activityPersona"
+                     :size="(activityPersonas.length > 1 || isCompact) ? PersonaSize.size16 : PersonaSize.size32"
+                     :style="personaStyle" />
+      </div>
+      <template v-else-if="$slots.icon"><slot name="icon" /></template>
     </div>
+
     <div :class="classNames.activityContent">
-      <span :class="classNames.activityText">
-        <slot name="description" />
+      <span v-if="$slots.activityDescription" :class="classNames.activityText">
+        <slot name="activityDescription" />
       </span>
-      <div v-if="$slots.comments" :class="classNames.commentText">
+      <div v-if="!isCompact && $slots.comments" :class="classNames.commentText">
         <slot name="comments" />
       </div>
-      <div :class="classNames.timeStamp">
+      <div v-if="!isCompact && $slots.timeStamp" :class="classNames.timeStamp">
         <slot name="timeStamp" />
       </div>
     </div>
@@ -23,10 +34,13 @@ import BaseComponent from '../BaseComponent'
 import { getStyles } from './ActivityItem.styles'
 import { IActivityItemStyles } from './ActivityItem.types'
 import { classNamesFunction } from '@uifabric-vue/utilities'
+import { IActivityItemClassNames, getClassNames } from './ActivityItem.classNames'
+import { PersonaCoin } from '../Persona/PersonaCoin'
+import { PersonaSize } from '../Persona/Persona.types'
 
-const getClassNames = classNamesFunction()
-
-@Component
+@Component({
+  components: { PersonaCoin },
+})
 export default class ActivityItem extends BaseComponent {
   @Prop({ type: Array, default: () => [] }) activityPersonas!: any[]
   @Prop({ type: Boolean, default: false }) animateBeaconSignal!: boolean
@@ -34,12 +48,33 @@ export default class ActivityItem extends BaseComponent {
   @Prop({ type: String, default: null }) beaconColorTwo!: string
   @Prop({ type: Boolean, default: false }) isCompact!: boolean
 
+  PersonaSize = PersonaSize
+
+  get personaStyle () {
+    if (!this.isCompact) return {}
+    return {
+      display: 'inline-block',
+      width: '10px',
+      minWidth: '10px',
+      overflow: 'visible',
+    }
+  }
+
+  get personasToRender () {
+    const personaLimit = this.isCompact ? 3 : 4
+
+    return this.activityPersonas
+      .filter((person, index) => index < personaLimit)
+  }
+
   get classNames (): any {
-    const { className, activityPersonas, animateBeaconSignal, beaconColorOne, beaconColorTwo, isCompact } = this
+    const { styles, theme, className, activityPersonas, animateBeaconSignal, beaconColorOne, beaconColorTwo, isCompact } = this
 
     return getClassNames(
-      () => getStyles(undefined, this.styles, animateBeaconSignal, beaconColorOne, beaconColorTwo, isCompact) as any,
-      {}
+      getStyles(theme, styles, animateBeaconSignal, beaconColorOne, beaconColorTwo, isCompact),
+      className!,
+      activityPersonas!,
+      isCompact!
     )
   }
 }
