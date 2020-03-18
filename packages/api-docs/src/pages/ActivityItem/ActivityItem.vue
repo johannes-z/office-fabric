291bd2ad745c5b4c5f6a9db9ecc3d542d1781c8d
johannes-z <johannes.zwirchmayr@gmail.com>
2020-03-18 13:56:37 +0100

    add more activityitem examples

--- packages/api-docs/src/pages/ActivityItem/ActivityItem.vue
+++ packages/api-docs/src/pages/ActivityItem/ActivityItem.vue
@@ -11,60 +11,15 @@
 
     <div class="content--inner ms-depth-8">
       <h2>Usage</h2>
-      <h2>Default ActivityItem</h2>
 
-      <f-activity-item>
-        <template #icon>
-          <f-icon icon-name="Message" />
-        </template>
-        <template #description>
-          <f-link href="#">Philippe Lampros</f-link> commented
-        </template>
-        <template #comments>
-          I am making a comment and mentioning <f-link href="#">@Anđela Debeljak</f-link>
-          in the text of the comment.
-        </template>
-        <template #timeStamp>
-          Just now
-        </template>
-      </f-activity-item>
+      <h3>Activity Items with Icons</h3>
+      <ActivityItemBasicExample />
 
-      <f-activity-item>
-        <template #icon>
-          <f-icon icon-name="Delete" />
-        </template>
-        <template #description>
-          <f-link href="#">Lisha Refai</f-link> deleted <b>DocumentTitle.docx</b>
-        </template>
-        <template #timeStamp>
-          Just now
-        </template>
-      </f-activity-item>
+      <h3>Activity Items with Personas</h3>
+      <ActivityItemPersonaExample />
 
-      <f-activity-item>
-        <template #icon>
-          <f-icon icon-name="FabricMovetoFolder" />
-        </template>
-        <template #description>
-          <f-link href="#">Julian Arvidsson</f-link> moved
-          <f-link>PresentationTitle.pptx</f-link> to
-          <f-link>Destination Folder</f-link>
-        </template>
-        <template #timeStamp>
-          Yesterday
-        </template>
-      </f-activity-item>
-
-      <f-activity-item comments="Test">
-        <template #icon>
-          <f-icon icon-name="FabricMovetoFolder" />
-        </template>
-        <template #description>
-          <f-link href="#">Julian Arvidsson</f-link> moved
-          <f-link>PresentationTitle.pptx</f-link> to
-          <f-link>Destination Folder</f-link>
-        </template>
-      </f-activity-item>
+      <h3>Compact Activity Items</h3>
+      <ActivityItemCompactExample />
     </div>
 
     <div class="content--inner ms-depth-8">
@@ -75,45 +30,19 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import { mergeStyleSets } from '@uifabric/styling'
+
+import ActivityItemBasicExample from './examples/ActivityItem.Basic.Example.vue'
+import ActivityItemCompactExample from './examples/ActivityItem.Compact.Example.vue'
+import ActivityItemPersonaExample from './examples/ActivityItem.Persona.Example.vue'
 
 @Component({
   components: {
+    ActivityItemBasicExample,
+    ActivityItemCompactExample,
+    ActivityItemPersonaExample,
   },
 })
 export default class ActivityItemPage extends Vue {
 }
 </script>
-
-<style lang="scss" module>
-.header {
-  padding-top: 18px;
-  padding-right: 24px;
-  padding-bottom: 12px;
-  padding-left: 24px;
-}
-.title {
-  font-size: 20px;
-  font-weight: 300;
-  margin-top: 0px;
-  margin-right: 0px;
-  margin-bottom: 0px;
-  margin-left: 0px;
-  color: rgb(50, 49, 48);
-}
-.inner {
-  height: 100%;
-  padding-top: 0px;
-  padding-right: 24px;
-  padding-bottom: 20px;
-  padding-left: 24px;
-}
-.subtext {
-  font-size: 12px;
-  font-weight: 300;
-  margin-top: 0px;
-  margin-right: 0px;
-  margin-bottom: 0px;
-  margin-left: 0px;
-  color: rgb(50, 49, 48);
-}
-</style>
---
+++ packages/api-docs/src/pages/ActivityItem/examples/ActivityItem.Basic.Example.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <f-activity-item :class="classNames.exampleRoot">
+      <template #icon>
+        <f-icon icon-name="Message" />
+      </template>
+      <template #activityDescription>
+        <f-link :class="classNames.nameText">Philippe Lampros</f-link> commented
+      </template>
+      <template #comments>
+        <span>I am making a comment and mentioning </span>
+        <f-link :class="classNames.nameText">@Anđela Debeljak</f-link>
+        <span> in the text of the comment.</span>
+      </template>
+      <template #timeStamp>
+        Just now
+      </template>
+    </f-activity-item>
+
+    <f-activity-item :class="classNames.exampleRoot">
+      <template #icon>
+        <f-icon icon-name="Delete" />
+      </template>
+      <template #activityDescription>
+        <f-link :class="classNames.nameText">Lisha Refai</f-link> deleted <b>DocumentTitle.docx</b>
+      </template>
+      <template #timeStamp>
+        Just now
+      </template>
+    </f-activity-item>
+
+    <f-activity-item :class="classNames.exampleRoot">
+      <template #icon>
+        <f-icon icon-name="FabricMovetoFolder" />
+      </template>
+      <template #activityDescription>
+        <f-link :class="classNames.nameText">Julian Arvidsson</f-link>
+        moved
+        <f-link :class="classNames.nameText">PresentationTitle.pptx</f-link>
+        to
+        <f-link :class="classNames.nameText">Destination Folder</f-link>
+      </template>
+      <template #timeStamp>
+        Yesterday
+      </template>
+    </f-activity-item>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { mergeStyleSets } from '@uifabric/styling'
+
+@Component({
+  components: {},
+})
+export default class ActivityItemBasicExample extends Vue {
+  classNames = mergeStyleSets({
+    exampleRoot: {
+      marginTop: '20px',
+    },
+    nameText: {
+      fontWeight: 'bold',
+    },
+  });
+}
+</script>
