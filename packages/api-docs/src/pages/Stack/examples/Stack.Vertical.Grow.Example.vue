192b2665cdd8acc0b52d8d90408f38de7273ce1f
johannes-z <johannes.zwirchmayr@gmail.com>
2020-03-18 13:56:53 +0100

    fix stack example

--- packages/api-docs/src/pages/Stack/examples/Stack.Vertical.Grow.Example.vue
+++ packages/api-docs/src/pages/Stack/examples/Stack.Vertical.Grow.Example.vue
@@ -1,53 +1,53 @@
+<template>
+  <Stack :tokens="outerStackTokens">
+    <Stack :styles="stackStyles" :tokens="innerStackTokens">
+      <StackItem :grow="3" :styles="stackItemStyles">
+        Grow is 3
+      </StackItem>
+      <StackItem :grow="2" :styles="stackItemStyles">
+        Grow is 2
+      </StackItem>
+      <StackItem grow :styles="stackItemStyles">
+        Grow is 1
+      </StackItem>
+    </Stack>
+  </Stack>
+</template>
+
 <script lang="tsx">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import { DefaultPalette } from '@uifabric/styling'
 import { Stack, StackItem, IStackTokens } from '@uifabric-vue/office-ui-fabric-vue'
 import { CreateElement } from 'vue'
 
-// Styles definition
-const stackStyles: any = {
-  root: {
-    background: DefaultPalette.themeTertiary,
-    height: 250,
-  },
-}
-const stackItemStyles: any = {
-  root: {
-    alignItems: 'center',
-    background: DefaultPalette.themePrimary,
-    color: DefaultPalette.white,
-    display: 'flex',
-    justifyContent: 'center',
-  },
-}
-
-// Tokens definition
-const outerStackTokens: IStackTokens = { childrenGap: 5 }
-const innerStackTokens: IStackTokens = {
-  childrenGap: 5,
-  padding: 10,
-}
-
-export default Vue.extend({
+@Component({
   components: { Stack, StackItem },
-  render (h: CreateElement) {
-    return (
-      <Stack tokens={outerStackTokens}>
-        <Stack styles={stackStyles} tokens={innerStackTokens}>
-          <StackItem grow={3} styles={stackItemStyles}>
-          Grow is 3
-          </StackItem>
-          <StackItem grow={2} styles={stackItemStyles}>
-          Grow is 2
-          </StackItem>
-          <StackItem grow styles={stackItemStyles}>
-          Grow is 1
-          </StackItem>
-        </Stack>
-      </Stack>
-    )
-  },
 })
+export default class StackVerticalGrowExample extends Vue {
+  // Styles definition
+  stackStyles: any = {
+    root: {
+      background: DefaultPalette.themeTertiary,
+      height: 250,
+    },
+  }
+  stackItemStyles: any = {
+    root: {
+      alignItems: 'center',
+      background: DefaultPalette.themePrimary,
+      color: DefaultPalette.white,
+      display: 'flex',
+      justifyContent: 'center',
+    },
+  }
+
+  // Tokens definition
+  outerStackTokens: IStackTokens = { childrenGap: 5 }
+  innerStackTokens: IStackTokens = {
+    childrenGap: 5,
+    padding: 10,
+  }
+}
 </script>
 
 <style lang="scss" scoped>
