c8c0b4f2fdb07ac67b08765c6c198e82bc3164e1
johannes-z <johannes.zwirchmayr@gmail.com>
2020-03-18 13:57:58 +0100

    add Link types

--- packages/office-ui-fabric-vue/src/components/Link/Link.types.ts
+++ packages/office-ui-fabric-vue/src/components/Link/Link.types.ts
@@ -1,8 +1,64 @@
-import { IStyle, IStyleFunctionOrObject } from '@uifabric/merge-styles'
-import { ITheme } from '@uifabric/styling'
+import LinkBase from './Link'
 
-export interface ILinkProps {
-  className?: string
+import { IStyle, ITheme } from '@uifabric/styling'
+import { IStyleFunctionOrObject } from '@uifabric/merge-styles'
+import { Component } from 'vue'
+// import { IKeytipProps } from '../../Keytip'
+
+/**
+ * {@docCategory Link}
+ */
+export interface ILink {
+  /** Sets focus to the link. */
+  focus(): void;
+}
+
+/**
+ * {@docCategory Link}
+ */
+export interface ILinkHTMLAttributes<T> {
+  // Shared
+  type?: string;
+
+  // Anchor
+  download?: any;
+  href?: string;
+  hrefLang?: string;
+  media?: string;
+  rel?: string;
+  target?: string;
+
+  // Button
+  autoFocus?: boolean;
+  disabled?: boolean;
+  form?: string;
+  formAction?: string;
+  formEncType?: string;
+  formMethod?: string;
+  formNoValidate?: boolean;
+  formTarget?: string;
+  name?: string;
+  value?: string | string[] | number;
+
+  // Any other props for HTMLElements or a React component passed to as=
+  [index: string]: any;
+}
+
+/**
+ * {@docCategory Link}
+ */
+export interface ILinkProps extends ILinkHTMLAttributes<HTMLAnchorElement | HTMLButtonElement | HTMLElement | LinkBase> {
+  /**
+   * Optional callback to access the ILink interface. Use this instead of ref for accessing
+   * the public methods and properties of the component.
+   */
+  componentRef?: IRefObject<ILink>;
+
+  /**
+   * Whether the link is disabled
+   */
+  disabled?: boolean;
+
   /**
    * Call to provide customized styling that will layer on top of the variant rules.
    */
@@ -12,12 +68,16 @@ export interface ILinkProps {
    * Theme (provided through customization.)
    */
   theme?: ITheme;
-  href?: string;
 
   /**
-   * Whether the link is disabled
+   * A component that should be used as the root element of the link returned from the Link component.
    */
-  disabled?: boolean;
+  as?: string | Component;
+
+  /**
+   * Optional keytip for this Link
+   */
+  keytipProps?: IKeytipProps;
 }
 
 /**
