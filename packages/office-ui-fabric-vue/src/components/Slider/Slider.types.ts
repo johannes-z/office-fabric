937bdea4d46f7403c73be12821773c0bc158a133
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 14:06:52 +0200

    add Slider tsx

--- packages/office-ui-fabric-vue/src/components/Slider/Slider.types.ts
+++ packages/office-ui-fabric-vue/src/components/Slider/Slider.types.ts
@@ -1,9 +1,142 @@
-import { IStyle } from '@uifabric/merge-styles'
+import { IStyleFunctionOrObject, IStyle } from '@uifabric/merge-styles'
+import { ITheme } from '@uifabric/styling'
 
-export interface ISliderProps {
+/**
+ * {@docCategory Slider}
+ */
+export interface ISlider {
+  value: number | undefined;
 
+  focus: () => void;
 }
 
+/**
+ * {@docCategory Slider}
+ */
+export interface ISliderProps {
+  /**
+   * Call to provide customized styling that will layer on top of the variant rules.
+   */
+  styles?: IStyleFunctionOrObject<ISliderStyleProps, ISliderStyles>;
+
+  /**
+   * Theme provided by High-Order Component.
+   */
+  theme?: ITheme;
+
+  /**
+   * Description label of the Slider
+   */
+  label?: string;
+
+  /**
+   * The initial value of the Slider. Use this if you intend for the Slider to be an uncontrolled component.
+   * This value is mutually exclusive to value. Use one or the other.
+   */
+  defaultValue?: number;
+
+  /**
+   * The initial value of the Slider. Use this if you intend to pass in a new value as a result of onChange events.
+   * This value is mutually exclusive to defaultValue. Use one or the other.
+   */
+  value?: number;
+
+  /**
+   * The min value of the Slider
+   * @defaultvalue 0
+   */
+  min?: number;
+
+  /**
+   * The max value of the Slider
+   * @defaultvalue 10
+   */
+  max?: number;
+
+  /**
+   * The difference between the two adjacent values of the Slider
+   * @defaultvalue 1
+   */
+  step?: number;
+
+  /**
+   * Whether to show the value on the right of the Slider.
+   * @defaultvalue true
+   */
+  showValue?: boolean;
+
+  /**
+   * Callback when the value has been changed
+   */
+  onChange?: (value: number) => void;
+
+  /**
+   * Callback on mouse up or touch end
+   */
+  onChanged?: (event: MouseEvent | TouchEvent | KeyboardEvent, value: number) => void;
+
+  /**
+   * A description of the Slider for the benefit of screen readers.
+   */
+  ariaLabel?: string;
+
+  /**
+   * A text description of the Slider number value for the benefit of screen readers.
+   * This should be used when the Slider number value is not accurately represented by a number.
+   */
+  ariaValueText?: (value: number) => string;
+  /**
+   * Optional flag to render the slider vertically. Defaults to rendering horizontal.
+   */
+  vertical?: boolean;
+
+  /**
+   * Optional flag to render the Slider as disabled.
+   * @defaultvalue false
+   */
+  disabled?: boolean;
+
+  /**
+   * Optional flag to decide that thumb will snap to closest value while moving the slider
+   * @defaultvalue false
+   */
+  snapToStep?: boolean;
+
+  /**
+   * Optional className to attach to the slider root element.
+   */
+  className?: string;
+
+  /**
+   * Optional mixin for additional props on the thumb button within the slider.
+   */
+  buttonProps?: any;
+
+  /**
+   * Optional function to format the slider value.
+   */
+  valueFormat?: (value: number) => string;
+
+  /**
+   * Optional flag to attach the origin of slider to zero. Helpful when the range include negatives.
+   * @defaultvalue false
+   */
+  originFromZero?: boolean;
+}
+
+/**
+ * {@docCategory Slider}
+ */
+export type ISliderStyleProps = Required<Pick<ISliderProps, 'theme'>> &
+Pick<ISliderProps, 'className' | 'disabled' | 'vertical'> & {
+  showTransitions?: boolean;
+  showValue?: boolean;
+  titleLabelClassName?: string;
+};
+
+/**
+ * {@docCategory Slider}
+ */
 export interface ISliderStyles {
   /**
    * Style set for the root element.
