b5dee536596506c3bb5ac3246029707f8a71bee1
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 13:37:47 +0200

    fix Link types

--- packages/office-ui-fabric-vue/src/components/Link/index.ts
+++ packages/office-ui-fabric-vue/src/components/Link/index.ts
@@ -1,11 +1,11 @@
 import { styled } from '../styled'
-import LinkBase from './Link'
+import { LinkBase } from './Link.base'
 import { getStyles } from './Link.styles'
 import { VueConstructor } from 'vue'
 
 export * from './Link.types'
 
-export const Link: VueConstructor = styled(
+export const Link = styled(
   LinkBase,
   getStyles,
   undefined,
