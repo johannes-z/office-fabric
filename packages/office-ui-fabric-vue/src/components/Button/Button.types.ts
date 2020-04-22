531333bf6ca5b4f78821b06e7263ad4077159261
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 17:31:53 +0200

    update Button styles

--- packages/office-ui-fabric-vue/src/components/Button/Button.types.ts
+++ packages/office-ui-fabric-vue/src/components/Button/Button.types.ts
@@ -1,4 +1,326 @@
-import { IStyle } from '@uifabric/merge-styles'
+import { ITheme, IStyle } from '@uifabric/styling'
+import { IIconProps } from '../Icon'
+import { IButtonClassNames } from './BaseButton.classNames'
+import { ISplitButtonClassNames } from './SplitButton/SplitButton.classNames'
+import { KeyCodes } from '@uifabric-vue/utilities'
+
+/**
+ * {@docCategory Button}
+ */
+export interface IButton {
+  /**
+   * Sets focus to the button.
+   */
+  focus: () => void;
+
+  /**
+   * If there is a menu associated with this button and it is visible, this will dismiss the menu
+   */
+  dismissMenu: () => void;
+
+  /**
+   * If there is a menu associated with this button and it is visible, this will open the menu.
+   * Params are optional overrides to the ones defined in `menuProps` to apply to just this instance of
+   * opening the menu.
+   *
+   * @param shouldFocusOnContainer - override to the ContextualMenu shouldFocusOnContainer prop.
+   * BaseButton implementation defaults to 'undefined'.
+   * @param shouldFocusOnMount - override to the ContextualMenu shouldFocusOnMount prop. BaseButton implementation
+   * defaults to `true`.
+   */
+  openMenu: (shouldFocusOnContainer?: boolean, shouldFocusOnMount?: boolean) => void;
+}
+
+/**
+ * {@docCategory Button}
+ */
+// tslint:disable:deprecation
+export interface IButtonProps {
+  /**
+   * If provided, this component will be rendered as an anchor.
+   * @defaultvalue ElementType.anchor
+   */
+  href?: string;
+
+  /**
+   * Changes the visual presentation of the button to be emphasized (if defined)
+   * @defaultvalue false
+   */
+  primary?: boolean;
+
+  /**
+   * Unique id to identify the item. Typically a duplicate of key value.
+   */
+  uniqueId?: string | number;
+
+  /**
+   * Whether the button is disabled
+   */
+  disabled?: boolean;
+
+  /**
+   * Whether the button can have focus in disabled mode
+   */
+  allowDisabledFocus?: boolean;
+
+  /**
+   * If set to true and if this is a splitButton (split == true) then the primary action of a split button is disabled.
+   */
+  primaryDisabled?: boolean;
+
+  /**
+   * Custom styling for individual elements within the button DOM.
+   */
+  styles?: IButtonStyles;
+
+  /**
+   * Theme provided by HOC.
+   */
+  theme?: ITheme;
+
+  /**
+   * Whether the button is checked
+   */
+  checked?: boolean;
+
+  /**
+   * Whether button is a toggle button with distinct on and off states. This should be true for buttons that permanently
+   * change state when a press event finishes, such as a volume mute button.
+   */
+  toggle?: boolean;
+
+  /**
+   * If provided, additional class name to provide on the root element.
+   */
+  className?: string;
+
+  /**
+   * The aria label of the button for the benefit of screen readers.
+   */
+  ariaLabel?: string;
+
+  /**
+   * Detailed description of the button for the benefit of screen readers.
+   *
+   * Besides the compound button, other button types will need more information provided to screen reader.
+   */
+  ariaDescription?: string;
+
+  /**
+   * If provided and is true it adds an 'aria-hidden' attribute instructing screen readers to ignore the element.
+   */
+  ariaHidden?: boolean;
+
+  /**
+   * Text to render button label. If text is supplied, it will override any string in button children.
+   * Other children components will be passed through after the text.
+   */
+  text?: string;
+
+  /**
+   * The props for the icon shown in the button.
+   */
+  iconProps?: IIconProps;
+
+  /**
+   * Props for button menu. Providing this will default to showing the menu icon. See menuIconProps for overriding
+   * how the default icon looks. Providing this in addition of onClick and setting the split property to true will
+   * render a SplitButton.
+   */
+  menuProps?: IContextualMenuProps;
+
+  /**
+   * Callback that runs after Button's contextualmenu was closed (removed from the DOM)
+   */
+  onAfterMenuDismiss?: () => void;
+
+  /**
+   * If set to true, and if menuProps and onClick are provided, the button will render as a SplitButton.
+   * @defaultvalue false
+   */
+  split?: boolean;
+
+  /**
+   * The props for the icon shown when providing a menu dropdown.
+   */
+  menuIconProps?: IIconProps;
+
+  /**
+   * Accessible label for the dropdown chevron button if this button is split.
+   */
+  splitButtonAriaLabel?: string;
+
+  /**
+   * Optional callback when menu is clicked.
+   */
+  onMenuClick?: (ev?: MouseEvent | KeyboardEvent, button?: IButtonProps) => void;
+
+  /**
+   * Custom render function for the icon
+   */
+  onRenderIcon?: any;
+
+  /**
+   * Custom render function for the label text.
+   */
+  onRenderText?: any;
+
+  /**
+   * Custom render function for the desciption text.
+   */
+  onRenderDescription?: any;
+
+  /**
+   * Custom render function for the aria description element.
+   */
+  onRenderAriaDescription?: any;
+
+  /**
+   * Custom render function for rendering the button children.
+   */
+  onRenderChildren?: any;
+
+  /**
+   * Custom render function for button menu icon
+   */
+  onRenderMenuIcon?: any;
+
+  /**
+   * Render a custom menu in place of the normal one.
+   */
+  menuAs?: any;
+
+  /**
+   * Description of the action this button takes.
+   * Only used for compound buttons
+   */
+  secondaryText?: string;
+
+  /**
+   * Any custom data the developer wishes to associate with the menu item.
+   */
+  data?: any;
+
+  /**
+   * Method to provide the classnames to style a button.
+   * The default value for this prop is the getClassnames func
+   * defined in BaseButton.classnames.
+   * @defaultvalue getBaseButtonClassNames
+   */
+  getClassNames?: (
+    theme: ITheme,
+    className: string,
+    variantClassName: string,
+    iconClassName: string | undefined,
+    menuIconClassName: string | undefined,
+    disabled: boolean,
+    checked: boolean,
+    expanded: boolean,
+    hasMenu: boolean,
+    isSplit: boolean | undefined,
+    allowDisabledFocus: boolean,
+  ) => IButtonClassNames;
+
+  /**
+   * Method to provide the classnames to style a button.
+   * The default value for this prop is the getClassnames func
+   * defined in BaseButton.classnames.
+   * @defaultvalue getBaseSplitButtonClassNames
+   */
+  getSplitButtonClassNames?: (
+    disabled: boolean,
+    expanded: boolean,
+    checked: boolean,
+    allowDisabledFocus: boolean,
+  ) => ISplitButtonClassNames;
+
+  /**
+   * Provides a custom KeyCode that can be used to open the button menu.
+   * The default KeyCode is the down arrow.
+   * A value of null can be provided to disable the key codes for opening the button menu.
+   */
+  menuTriggerKeyCode?: KeyCodes | null;
+
+  /**
+   * Optional keytip for this button
+   */
+  keytipProps?: IKeytipProps;
+
+  /**
+   * Menu will not be created or destroyed when opened or closed, instead it
+   * will be hidden. This will improve perf of the menu opening but could potentially
+   * impact overall perf by having more elements in the dom. Should only be used
+   * when perf is important.
+   * Note: This may increase the amount of time it takes for the button itself to mount.
+   */
+  persistMenu?: boolean;
+
+  /**
+   * If true, the persisted menu is rendered hidden when the button
+   * initially mounts. Non-persisted menus will
+   * not be in the component tree unless they are being shown
+   *
+   * Note: This increases the time the button will take to mount, but
+   * can improve perceived menu open perf. when the user opens the menu.
+   *
+   * @defaultvalue undefined, equivalent to false
+   *
+   * @deprecated There is known bug in Edge when this prop is true where scrollbars
+   * overlap with the content when a menu is first rendered hidden.
+   * See: https://github.com/microsoft/fluentui/issues/9034
+   * Please do not start using this. If you are already using this,
+   * please make sure that you are doing so only in non-Edge browsers
+   */
+  renderPersistedMenuHiddenOnMount?: boolean;
+
+  /**
+   * Experimental prop that get passed into the menuButton that's rendered as part of
+   * split button. Anything passed in will likely need to have accompanying
+   * style changes.
+   */
+  splitButtonMenuProps?: IButtonProps;
+
+  /**
+   * Style for the description text if applicable (for compound buttons.)
+   * Deprecated, use `secondaryText` instead.
+   * @deprecated Use `secondaryText` instead.
+   */
+  description?: IStyle;
+
+  /**
+   * yet unknown docs
+   */
+  defaultRender?: any;
+
+  /**
+   * Optional props to be applied only to the primary action button of SplitButton and not to the
+   * overall SplitButton container
+   */
+  primaryActionButtonProps?: IButtonProps;
+}
+
+/**
+ * {@docCategory Button}
+ */
+export enum ElementType {
+  /** <button> element. */
+  button = 0,
+  /** <a> element. */
+  anchor = 1,
+}
+
+/**
+ * {@docCategory Button}
+ */
+export enum ButtonType {
+  normal = 0,
+  primary = 1,
+  hero = 2,
+  compound = 3,
+  command = 4,
+  icon = 5,
+  default = 6,
+}
 
 /**
  * {@docCategory Button}
