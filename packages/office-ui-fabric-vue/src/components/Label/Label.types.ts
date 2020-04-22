c636acb9581f78a11e1dfcf5574bce26da5f0bb0
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 22:21:31 +0200

    add Label types fix type errors

--- packages/office-ui-fabric-vue/src/components/Label/Label.types.ts
+++ packages/office-ui-fabric-vue/src/components/Label/Label.types.ts
@@ -1,6 +1,20 @@
-import { IStyle } from '@uifabric/merge-styles'
+import { ITheme } from '@uifabric/styling'
+import { IStyleFunctionOrObject, IStyle } from '@uifabric/merge-styles'
 
+/**
+ * {@docCategory Label}
+ */
+export interface ILabel {}
+
+/**
+ * {@docCategory Label}
+ */
 export interface ILabelProps {
+  /**
+   * Render the root element as another type.
+   */
+  as?: any;
+
   /**
    * Whether the associated form field is required or not
    * @defaultvalue false
@@ -11,11 +25,37 @@ export interface ILabelProps {
    * Renders the label as disabled.
    */
   disabled?: boolean;
+
+  /**
+   * Theme provided by HOC.
+   */
+  theme?: ITheme;
+
+  /**
+   * Styles for the label.
+   */
+  styles?: IStyleFunctionOrObject<ILabelStyleProps, ILabelStyles>;
 }
 
+/**
+ * {@docCategory Label}
+ */
 export interface ILabelStyles {
   /**
    * Styles for the root element.
    */
   root: IStyle;
+}
+
+/**
+ * {@docCategory Label}
+ */
+export interface ILabelStyleProps {
+  /**
+   *
+   */
+  theme: ITheme;
+  className?: string;
+  disabled?: boolean;
+  required?: boolean;
 }
