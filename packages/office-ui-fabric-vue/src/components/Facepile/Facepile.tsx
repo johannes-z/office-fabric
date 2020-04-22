4fed9b18567b4803d537cc593add3ee7d1199765
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 22:00:34 +0200

    add FacepileButton tsx

--- packages/office-ui-fabric-vue/src/components/Facepile/Facepile.tsx
+++ packages/office-ui-fabric-vue/src/components/Facepile/Facepile.tsx
@@ -1,11 +1,8 @@
 import { styled } from '../styled'
 import { FacepileBase } from './Facepile.base'
 import { getStyles } from './Facepile.styles'
-import { VueConstructor } from 'vue'
 
-// export * from './Facepile.types'
-
-export const Facepile: VueConstructor = styled(
+export const Facepile = styled(
   FacepileBase,
   getStyles,
   undefined,
