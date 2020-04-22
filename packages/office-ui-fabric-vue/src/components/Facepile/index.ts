ac045e5f300e5adf495468a106ebf210901aec84
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-22 17:31:38 +0200

    add Facepile tsx

--- packages/office-ui-fabric-vue/src/components/Facepile/index.ts
+++ packages/office-ui-fabric-vue/src/components/Facepile/index.ts
@@ -1,13 +1,3 @@
-import { styled } from '../styled'
-import FacepileBase from './Facepile.vue'
-import { getStyles } from './Facepile.styles'
-import { VueConstructor } from 'vue'
-
-// export * from './Facepile.types'
-
-export const Facepile: VueConstructor = styled(
-  FacepileBase,
-  getStyles,
-  undefined,
-  { scope: 'Facepile' },
-)
+export * from './Facepile.types'
+export * from './Facepile.base'
+export * from './Facepile'
