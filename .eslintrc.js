171198f8515874bf40b1f4147f602cbf8b3414cf
johannes-z <johannes.zwirchmayr@gmail.com>
2020-04-23 08:04:55 +0200

    update eslint eslint workaround for false-postivies in no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     }],
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-unused-vars": "off",
   },
 
   parserOptions: {
