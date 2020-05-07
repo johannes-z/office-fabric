f39bde49fbfce4ad2733e8280e55a029b64e8e04
johannes-z <johannes.zwirchmayr@gmail.com>
2020-05-07 10:51:04 +0200

    add Shimmer tsx

--- packages/office-ui-fabric-vue/src/components/Shimmer/Shimmer.types.ts
+++ packages/office-ui-fabric-vue/src/components/Shimmer/Shimmer.types.ts
@@ -1,8 +1,168 @@
-import { IStyle } from '@uifabric/merge-styles'
+import { IStyleFunctionOrObject, IStyle } from '@uifabric/merge-styles'
+import { ITheme } from '@uifabric/styling'
 
+/**
+ * {@docCategory Shimmer}
+ */
+export interface IShimmer {}
+
+/**
+ * Shimmer component props.
+ * {@docCategory Shimmer}
+ */
 export interface IShimmerProps {
+
+  /**
+   * Sets the width value of the shimmer wave wrapper.
+   * @defaultvalue 100%
+   */
+  width?: number | string;
+
+  /**
+   * Controls when the shimmer is swapped with actual data through an animated transition.
+   * @defaultvalue false
+   */
+  isDataLoaded?: boolean;
+
+  /**
+   * Elements to render in one line of the Shimmer.
+   */
+  shimmerElements?: IShimmerElement[];
+
+  /**
+   * Localized string of the status label for screen reader
+   */
+  ariaLabel?: string;
+
+  /**
+   * Call to provide customized styling that will layer on top of the variant rules.
+   */
+  styles?: IStyleFunctionOrObject<IShimmerStyleProps, IShimmerStyles>;
+
+  /**
+   * Additional CSS class(es) to apply to the Shimmer container.
+   */
+  className?: string;
+
+  /**
+   * Theme provided by High-Order Component.
+   */
+  theme?: ITheme;
+
+  /**
+   * Defines an object with possible colors to pass for Shimmer customization used on different backgrounds.
+   */
+  shimmerColors?: IShimmerColors;
 }
 
+/**
+ * Shimmer Elements Interface representing all common properties between Gap, Circle and Line.
+ * {@docCategory Shimmer}
+ */
+export interface IShimmerElement {
+  /**
+   * Represents the possible type of the shimmer elements: Gap, Circle, Line.
+   * Required for every element you intend to use.
+   */
+  type: ShimmerElementType;
+
+  /**
+   * Sets the height of the element (ICircle, ILine, IGap) in pixels.
+   * Read more details for each specific element.
+   */
+  height?: number;
+
+  /**
+   * Sets the width value of the element (ILine, IGap) in pixels.
+   * Read more details for each specific element.
+   */
+  width?: number | string;
+
+  /**
+   * Sets vertical alignment of the element (ICircle, ILine).
+   * @defaultvalue center
+   */
+  verticalAlign?: 'top' | 'center' | 'bottom';
+}
+
+/**
+ * Line element interface
+ * {@docCategory Shimmer}
+ */
+export interface ILine extends IShimmerElement {
+  /**
+   * Sets the height of the shimmer line in pixels.
+   * @defaultvalue 16px
+   */
+  height?: number;
+
+  /**
+   * Line width value.
+   * @defaultvalue 100%
+   */
+  width?: number | string;
+}
+
+/**
+ * Circle element interface
+ * {@docCategory Shimmer}
+ */
+export interface ICircle extends IShimmerElement {
+  /**
+   * Sets the height of the shimmer circle in pixels.
+   * Minimum supported 10px.
+   * @defaultvalue 24px
+   */
+  height?: number;
+}
+
+/**
+ * Gap element interface
+ * {@docCategory Shimmer}
+ */
+export interface IGap extends IShimmerElement {
+  /**
+   * Sets the height of the shimmer gap in pixels.
+   * @defaultvalue 16px
+   */
+  height?: number;
+
+  /**
+   * Gap width value.
+   * @defaultvalue 10px
+   */
+  width?: number | string;
+}
+
+/**
+ * Defines props needed to construct styles.
+ * This represents the simplified set of immutable things which control the class names.
+ * {@docCategory Shimmer}
+ */
+export interface IShimmerStyleProps {
+  /** Boolean flag to trigger fadeIn/fadeOut transition animation when content is loaded. */
+  isDataLoaded?: boolean;
+
+  /** Optional CSS class name for the component attached to the root stylable area. */
+  className?: string;
+
+  /** Theme provided by High-Order Component. */
+  theme: ITheme;
+
+  /** Interval in milliseconds for the adeIn/fadeOut transition animation. */
+  transitionAnimationInterval?: number;
+
+  /** Color to be used as the main background color of Shimmer when not animating. */
+  shimmerColor?: string;
+
+  /** Tip color of the shimmer wave which gradually gets from and to `shimmerColor`. */
+  shimmerWaveColor?: string;
+}
+
+/**
+ * Represents the stylable areas of the control.
+ * {@docCategory Shimmer}
+ */
 export interface IShimmerStyles {
   /** Refers to the root wrapper element. */
   root?: IStyle;
@@ -18,6 +178,27 @@ export interface IShimmerStyles {
 
   /** Styles for the hidden helper element to aid with screen readers. */
   screenReaderText?: IStyle;
+}
+
+/**
+ * Describes the possible types for shimmer elements used.
+ * {@docCategory Shimmer}
+ */
+export enum ShimmerElementType {
+  /**
+   * Line element type
+   */
+  line = 1,
+
+  /**
+   * Circle element type
+   */
+  circle = 2,
+
+  /**
+   * Gap element type
+   */
+  gap = 3,
 }
 
 /**
@@ -38,5 +219,30 @@ export enum ShimmerElementsDefaultHeights {
   /**
    * Default height of the circle element when not provided by user: 24px
    */
-  circle = 24
+  circle = 24,
+}
+
+/**
+ * Interface describing the possible color customizations of Shimmer.
+ * {@docCategory Shimmer}
+ */
+export interface IShimmerColors {
+  /**
+   * Defines the main background color which is the color you see when the wave is not animating.
+   * @defaultvalue theme.palette.neutralLight
+   */
+  shimmer?: string;
+
+  /**
+   * Defines the tip color of the shimmer wave which gradually gets from and to `shimmer` color.
+   * @defaultvalue theme.palette.neutralLighter
+   */
+  shimmerWave?: string;
+
+  /**
+   * Defines the background color of the space in between and around shimmer elements (borders, gaps and
+   * rounded corners).
+   * @defaultvalue theme.palette.white
+   */
+  background?: string;
 }
--- packages/office-ui-fabric-vue/src/components/Shimmer/index.ts
+++ packages/office-ui-fabric-vue/src/components/Shimmer/index.ts
@@ -1,13 +1,3 @@
-import { styled } from '../styled'
-import ShimmerBase from './Shimmer.vue'
-import { getStyles } from './Shimmer.styles'
-import { VueConstructor } from 'vue'
-
+export * from './Shimmer'
+export * from './Shimmer.base'
 export * from './Shimmer.types'
-
-export const Shimmer: VueConstructor = styled(
-  ShimmerBase,
-  getStyles,
-  undefined,
-  { scope: 'Shimmer' },
-)
