74279d7b1fbb201ce0c660cb570a02cdeb1592e7
johannes-z <johannes.zwirchmayr@gmail.com>
2020-10-19 16:15:37 +0200

    add VerticalDivider renderFn

--- packages/office-ui-fabric-vue/src/components/Divider/VerticalDivider.base.ts
+++ packages/office-ui-fabric-vue/src/components/Divider/VerticalDivider.base.ts
@@ -1,19 +1,17 @@
 import { IVerticalDividerProps, IVerticalDividerPropsStyles, IVerticalDividerStyles } from './VerticalDivider.types'
 import StatelessComponent from '../StatelessComponent'
-import { RenderContext } from 'vue'
+import { CreateElement, RenderContext } from 'vue'
 import { classNamesFunction } from '@uifabric-vue/utilities'
 
 const getClassNames = classNamesFunction<IVerticalDividerPropsStyles, IVerticalDividerStyles>()
 
 export class VerticalDividerBase extends StatelessComponent<IVerticalDividerProps> {
-  render (h, ctx: RenderContext) {
+  render (h: CreateElement, ctx: RenderContext) {
     const { styles, theme, className } = ctx.props
+    const classNames = getClassNames(styles, { theme, className })
 
-    const classNames = getClassNames(styles, { theme: theme, className })
-    return (
-      <span class={classNames.wrapper}>
-        <span class={classNames.divider} />
-      </span>
-    )
+    return h('span', { class: classNames.wrapper }, [
+      h('span', { class: classNames.divider }),
+    ])
   }
 }
