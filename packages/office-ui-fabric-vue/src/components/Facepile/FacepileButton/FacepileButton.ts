2e031ecbcc41d109f0f44bb2f8e5dba4b4f8e582
johannes-z <johannes.zwirchmayr@gmail.com>
2020-10-19 16:15:48 +0200

    add FacepileButton renderFn

--- packages/office-ui-fabric-vue/src/components/Facepile/FacepileButton/FacepileButton.ts
+++ packages/office-ui-fabric-vue/src/components/Facepile/FacepileButton/FacepileButton.ts
@@ -1,7 +1,8 @@
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import { getStyles } from './FacepileButton.styles'
 import BaseComponent from '../../BaseComponent'
-import { BaseButton } from '../../Button/'
+import { BaseButton } from '../../Button'
+import { CreateElement } from 'vue'
 
 @Component
 export class FacepileButton extends BaseComponent {
@@ -9,21 +10,19 @@ export class FacepileButton extends BaseComponent {
     return getStyles(this.theme, this.className, this.styles)
   }
 
-  render () {
+  render (h: CreateElement) {
     const props = {
       ...this.$props,
       variantClassName: 'ms-Button--facepile',
       styles: this.internalStyles,
     }
-    return (
-      <BaseButton {...{
-        props,
+    return h(
+      BaseButton, {
         attrs: this.$attrs,
+        props,
         on: this.$listeners,
         scopedSlots: this.$scopedSlots,
-      }}>
-        {this.$slots.flex}
-      </BaseButton>
+      }, this.$slots.flex,
     )
   }
 }
