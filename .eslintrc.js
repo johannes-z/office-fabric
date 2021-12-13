ada71a5a0f591572e7ea05b4289aa4e3e486efb7
johannes-z <johannes.zwirchmayr@gmail.com>
2021-12-13 08:16:27 +0100

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,53 @@
 module.exports = {
   root: true,
 
-  env: {
-    node: true,
-  },
-
   extends: [
     'plugin:vue/recommended',
     '@vue/standard',
     '@vue/typescript',
   ],
 
+  plugins: [
+    'eslint-plugin-html',
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
   rules: {
-    // fixes eslint crashes when parsing indents
-    indent: 'off',
+    indent: 0,
     '@typescript-eslint/indent': [
       'error',
       2,
     ],
-    'no-prototype-builtins': 0,
-    'no-use-before-define': 0,
+    quotes: [1, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    // unused vars
     'no-unused-vars': 0,
-    'standard/no-callback-literal': 0,
-    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'vue/no-unused-vars': 1,
+    '@typescript-eslint/no-unused-vars': [1, { args: 'none' }],
 
-    'generator-star-spacing': 'off',
+    // false positives
+    'vue/valid-v-slot': 0,
+
+    'generator-star-spacing': ['error', { before: false, after: true }],
     'comma-dangle': [
       'error',
       'always-multiline',
     ],
-    'vue/no-v-html': 0,
+    'vue/no-v-html': 1,
     'vue/singleline-html-element-content-newline': 0,
     'vue/html-closing-bracket-newline': ['error', { multiline: 'never' }],
+    'vue/first-attribute-linebreak': ['error', {
+      singleline: 'ignore',
+      multiline: 'beside',
+    }],
     'vue/max-attributes-per-line': ['error', {
       singleline: 2,
-      multiline: {
-        max: 1,
-        allowFirstLine: true,
-      },
+      multiline: 1,
     }],
-    'vue/no-parsing-error': [2, {
-      'x-invalid-end-tag': false,
-    }],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-parsing-error': 2,
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/export': 'off',
-  },
-
-  parserOptions: {
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
 }
