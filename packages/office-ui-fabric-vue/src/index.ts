d79bbbb979fb426dfc92450d24de291ecc55fe04
johannes-z <johannes.zwirchmayr@gmail.com>
2022-03-01 12:26:18 +0100

    refactor: update exports

--- packages/office-ui-fabric-vue/src/index.ts
+++ packages/office-ui-fabric-vue/src/index.ts
@@ -3,27 +3,27 @@
 import Fabric from './plugin/office-fabric'
 import * as Components from './components'
 
-import Vue from 'vue'
-
 export * from './components'
 export * from './components/ThemeProvider'
 
-export * from './Styling'
-export * from './FabricUtilities'
+export * from '@uifabric-vue/utilities'
 
 export * from './utilities'
 
 export * from './common/DirectionalHint'
 
+export { initializeIcons } from '@uifabric/icons'
+// export type { IIconSubset } from './plugin/registerIcons'
+// export { registerIcons } from './plugin/registerIcons'
+
+// eslint-disable-next-line import/export
+export * from '@uifabric/styling'
+// eslint-disable-next-line import/export
+export { loadTheme } from './plugin/loadTheme'
+export type { IPartialTheme, IOptions } from './plugin/office-fabric'
+
 export {
   Fabric,
   Components as FabricComponents,
 }
-export { initializeIcons } from '@uifabric/icons'
-export type { IIconSubset } from './plugin/registerIcons'
-export { registerIcons } from './plugin/registerIcons'
-
-export type { IPartialTheme, IOptions } from './plugin/office-fabric'
-export { loadTheme } from './plugin/loadTheme'
-
 export default Fabric
