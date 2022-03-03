5c6474546e9b2154fbceec08c2ffed18bdf4f5a5
johannes-z <johannes.zwirchmayr@gmail.com>
2022-03-03 15:46:34 +0100

    fix(@uifabric-vue/office-ui-fabric-vue): fix type for registerIcons

--- packages/office-ui-fabric-vue/src/plugin/registerIcons.ts
+++ packages/office-ui-fabric-vue/src/plugin/registerIcons.ts
@@ -1,11 +1,11 @@
 import { IRawStyle, IFontFace } from '@uifabric/merge-styles'
-import { CreateElement } from 'vue'
+import { CreateElement, VNode } from 'vue'
 import { registerIcons as originalRegisterIcons, IIconOptions } from '@uifabric/styling'
 
 export interface IIconSubset {
   fontFace?: IFontFace;
   icons: {
-    [key: string]: (h: CreateElement) => any | Element | string | JSX.Element;
+    [key: string]: (h: CreateElement) => any | Element | string | JSX.Element | VNode;
   };
   style?: IRawStyle;
 }
