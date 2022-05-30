ab8f075d5d87a7f3ba4811efee8ccac94814ce1e
johannes-z <johannes.zwirchmayr@gmail.com>
2022-05-30 15:42:07 +0200

    feat: export FileTypeIconMap

--- packages/@fluentui-vue/file-type-icons/src/index.ts
+++ packages/@fluentui-vue/file-type-icons/src/index.ts
@@ -3,3 +3,4 @@ export { initializeFileTypeIcons } from './initializeFileTypeIcons'
 export { FileTypeIconSize, IFileTypeIconOptions, ImageFileType, getFileTypeIconProps } from './getFileTypeIconProps'
 
 export { FileIconType } from './FileIconType'
+export { FileTypeIconMap } from './FileTypeIconMap'
