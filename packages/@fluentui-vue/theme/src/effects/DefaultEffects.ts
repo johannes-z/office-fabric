1914c333b312eb7e3ba7830f0401bd19c22fbfca
johannes-z <johannes.zwirchmayr@gmail.com>
2022-06-01 14:36:42 +0200

    feat: add @fluentui-vue/theme

---
+++ packages/@fluentui-vue/theme/src/effects/DefaultEffects.ts
@@ -0,0 +1,12 @@
+import { Depths } from './FluentDepths'
+
+export const DefaultEffects: any = {
+  elevation4: Depths.depth4,
+  elevation8: Depths.depth8,
+  elevation16: Depths.depth16,
+  elevation64: Depths.depth64,
+
+  roundedCorner2: '2px',
+  roundedCorner4: '4px',
+  roundedCorner6: '6px',
+}
---
+++ packages/@fluentui-vue/theme/src/index.ts
@@ -0,0 +1,11 @@
+export * from './Palette'
+export * from './SemanticColors'
+export * from './types/index'
+export * from './colors/index'
+export * from './effects/index'
+export * from './spacing/index'
+export * from './motion/index'
+export * from './fonts/index'
+
+export * from './createTheme'
+export { FluentTheme } from './FluentTheme'
