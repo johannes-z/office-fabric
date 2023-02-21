22f52cb2a79256260e94dcf4856c1aa945295779
johannes-z <johannes.zwirchmayr@gmail.com>
2023-02-21 13:56:27 +0100

    fix(Text): fix boolean props

--- packages/@fluentui-vue/next/src/components/Text/Text.base.ts
+++ packages/@fluentui-vue/next/src/components/Text/Text.base.ts
@@ -1,17 +1,17 @@
 import { classNamesFunction } from '@fluentui-vue/utilities'
 import { h } from 'vue'
-import type { ITextStyles } from './Text.types'
+import type { ITextProps, ITextStyles } from './Text.types'
 import { StylingPropKeys, asSlotProps } from '@/utils'
 
 const getClassNames = classNamesFunction<any, ITextStyles>()
 
-export const TextBase = (props, { attrs, slots }) => {
+export const TextBase = (props: ITextProps, { attrs, slots }) => {
   const { styles, theme, as: RootType = 'span', block, nowrap, variant = 'medium' } = props
 
   const classNames = getClassNames(styles, {
     theme,
-    block,
-    nowrap,
+    block: block != null && block !== false,
+    nowrap: nowrap != null && nowrap !== false,
     variant,
   })
 
