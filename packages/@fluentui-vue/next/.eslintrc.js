1e4d69948ff06878ead3f2f039042080785c6049
johannes-z <johannes.zwirchmayr@gmail.com>
2023-06-27 10:23:19 +0200

    chore: update dependencies

--- packages/@fluentui-vue/next/.eslintrc.js
+++ packages/@fluentui-vue/next/.eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: false,
 
   rules: {
+    'quote-props': ['error', 'as-needed'],
+    'vue/define-macros-order': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/prefer-ts-expect-error': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
