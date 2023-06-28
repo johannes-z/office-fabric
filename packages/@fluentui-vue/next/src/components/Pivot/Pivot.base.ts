312e52fd1d3ef5b61a910d0d4a3eefb8d82f137f
johannes-z <johannes.zwirchmayr@gmail.com>
2023-06-28 13:42:09 +0200

    feat(Pivot): add files

---
+++ packages/@fluentui-vue/next/src/components/Pivot/Pivot.base.ts
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue'
+
+export const PivotBase = defineComponent({
+  name: 'PivotBase',
+
+  props: {
+
+  },
+
+  setup(props, { attrs, slots }) {
+    return () => null
+  },
+})
---
+++ packages/@fluentui-vue/next/src/components/Pivot/Pivot.ts
@@ -0,0 +1,8 @@
+import { PivotBase } from './Pivot.base'
+import { getStyles } from './Pivot.styles'
+import { styled } from '@/components/styled'
+
+export const Pivot = styled(
+  PivotBase,
+  getStyles,
+)
